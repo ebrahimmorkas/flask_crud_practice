3ff1a26f7b83578882891db30579c0e2dd9e6666
ebrahimmorkas <109964591+ebrahimmorkas@users.noreply.github.com>
2024-01-19 22:37:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = "localhost"
 app.config['MYSQL_USER'] = "root"
-app.config['MYSQL_PASSWORD'] = "Ebrahimmm@17"
+app.config['MYSQL_PASSWORD'] = ""
 app.config['MYSQL_DB'] = "flaskdbprac"
 
 mysql = MySQL(app)
